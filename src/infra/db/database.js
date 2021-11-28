4b06cbbdb5ded37a99e346610db421a8c3328321
camilo penagos <penagoscamilo99@gmail.com>
2021-11-28 11:42:44 -0500

    feat: configurar mongoose client

---
+++ src/infra/db/database.js
@@ -0,0 +1,23 @@
+// Using ES6 imports
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+
+// Conexión a Base de datos
+ const connec = async()=> {
+  const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.6mdyq.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+  console.log("Intentando conexion");
+   return mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+      console.log("Base de datos conectada");
+      return true;
+    })
+    .catch((e) => {
+      console.log("error db:", e);
+      process.exit(1) //detener la app
+      return false;
+    });
+}
+
+module.exports = connec;
